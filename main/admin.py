6a10235fbcb20f2738e7c4951ccfd57066a7d6d2
Gabriel Kakizaki <gkakizaki@gmail.com>
2024-08-18 19:56:55 -0300

    Popular plantações e insumos

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Usuario
+from .models import Usuario, Plantacao, Insumo
 
 # Register your models here.
 
 admin.site.register(Usuario)
+admin.site.register(Plantacao)
+admin.site.register(Insumo)
